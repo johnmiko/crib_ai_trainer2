dd701697ce6f02aafb88a859384e4698784aa091
johnm <johnmiko4@gmail.com>
2025-12-26 16:01:13 -0700

    getting close to tests passing, need to see if it's actually working though

--- crib_ai_trainer/tests/test_determinism.py
+++ crib_ai_trainer/tests/test_determinism.py
@@ -2,13 +2,11 @@
 import random
 from cribbage.playingcards import Deck
 
-def test_seeded_sampling_determinism():
-    random.seed(123)
-    d1 = Deck()
+def test_seeded_sampling_determinism():    
+    d1 = Deck(123)
     d1.shuffle()
-    h1 = [d1.draw() for _ in range(6)]
-    random.seed(123)
-    d2 = Deck()
+    h1 = [d1.draw() for _ in range(6)]    
+    d2 = Deck(123)
     d2.shuffle()
     h2 = [d2.draw() for _ in range(6)]
     assert h1 == h2
--- crib_ai_trainer/tests/test_features.py
+++ crib_ai_trainer/tests/test_features.py
@@ -4,12 +4,9 @@ from crib_ai_trainer.features import multi_hot_cards
 
 
 def test_multi_hot_cards():
-    # hand = [Card('H', 1), Card('D', 13), Card('S', 7)]
-    # hand = [9♥, 2♠, 9♣, 3♣, 4♦, 7♦]
-    # hand = [Card('H', 9), Card('S', 2), Card('C', 9), Card('C', 3), Card('D', 4), Card('D', 7)]
-    hand = [Card(Deck.SUITS["hearts"],9), Card(Deck.SUITS["spades"],2), Card(Deck.SUITS["clubs"],9), Card(Deck.SUITS["clubs"],3), Card(Deck.SUITS["diamonds"],4), Card(Deck.SUITS["diamonds"],7)]
+    hand = [Card(Deck.RANKS["ace"], Deck.SUITS["hearts"]), Card(Deck.RANKS["king"], Deck.SUITS["diamonds"]), Card(Deck.RANKS["seven"], Deck.SUITS["spades"]),Card(Deck.RANKS["ace"], Deck.SUITS["spades"]), Card(Deck.RANKS["ace"], Deck.SUITS["diamonds"]),Card(Deck.RANKS["ace"], Deck.SUITS["clubs"])]
     vec = multi_hot_cards(hand)
-    assert vec.sum() == 3.0
+    assert vec.sum() == 6.0
     assert vec[0] == 1.0  # AH
-    assert vec[12] == 1.0  # KD
-    assert vec[32 + 6] == 1.0  # 7S
+    assert vec[12] == 0.0  # KD
+    assert vec[32 + 6] == 0.0  # 7S
