42783050482b78fc9718d57a3aac879ccb6e1325
johnm <johnmiko4@gmail.com>
2026-02-10 09:54:24 -0700

    delete unused files

--- crib_ai_trainer/old/logging.py
+++
@@ -1,9 +0,0 @@
-from logging import getLogger, basicConfig
-import os
-
-logger = getLogger(__name__)
-
-# Basic logging setup; can be customized by scripts
-if not os.environ.get("CRIB_LOGGING_CONFIGURED"):
-    basicConfig(level=os.environ.get("CRIB_LOG_LEVEL", "INFO"))
-    os.environ["CRIB_LOGGING_CONFIGURED"] = "1"
--- tests/test_smoke_game.py
+++
@@ -1,7 +0,0 @@
-from cribbage.cribbagegame import CribbageGame
-from crib_ai_trainer.players.random_player import RandomPlayer
-
-def test_smoke_game_valid_score():
-    g = CribbageGame(players=[RandomPlayer(), RandomPlayer()])
-    # crib_engine's CribbageGame does not have play_game(), so we just check instantiation
-    assert len(g.players) == 2
